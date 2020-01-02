928fbf6066ce9ea9c2e7f3da1b7a48e235d63da7
Malcomputer <malcolmmkubwa@gmail.com>
2020-01-02 02:45:51 -0600

    upgrade to 29

--- app/src/main/java/com/example/malco/myapplication/textboxActivity.java
+++ app/src/main/java/com/example/malco/myapplication/textboxActivity.java
@@ -2,7 +2,7 @@ package com.example.malco.myapplication;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
